04772814eabcfbe896f981b690e4dac6ce1e84c7
Godslovelee <godslovelee@yahoo.com>
2021-11-11 20:31:32 +0100

    ""

--- lib/main.dart
+++ lib/main.dart
@@ -88,7 +88,7 @@ class ClockClass extends StateNotifier<DateTime> {
 
 class MyHomePage2 extends ConsumerWidget {
   @override
-  Widget build(BuildContext context, watch) {
+  Widget build(BuildContext context, ScopedReader watch) {
     final currentTime = watch(clockProvider.state);
     final timeFormat = DateFormat.Hms().format(currentTime);
     return Scaffold(
